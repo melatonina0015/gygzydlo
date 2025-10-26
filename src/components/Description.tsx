7f94ded23339c846b291d9c92a2cfeba54f986be
melatonina0015 <kwam0015@gmail.com>
2025-10-26 23:06:40 +0100

    Back button

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -1,4 +1,8 @@
 const Description = () => {
+    const ReloadPage = () => {
+        window.location.reload();
+    }
+
     return (
         <div className='h-full flex justify-center items-center'>
             <div className='flex flex-col gap-[20px]'>
@@ -11,16 +15,21 @@ const Description = () => {
                 <ol className='list-decimal'>
                     <li><div>
                         <p>
-                            TEMAT: GYGŻYDŁO <br/> Projekt na pracownię Artbook i Komiks, dot. stworzenia serii ilustracji/rycin przedstawiających autorskie wyobrażenie nieistniejących stworzeń i formacji tkankowych. Rozszerzenie projektu o narrację transmedialną, stworzenie własnej creepypasty popartej istnieniem w sieci. No coś na zasadzie kampanii promocyjnej nowego sezonu Mr Robot albo nowego albumu Twenty One Pilots. Trzeba szukać easter eggów (capture the flag) wszędzie (social media, na ulicy) i po wpisywaniu haseł na stronie pokazują się kolejne elementy historii. Hasła znajdują się w random postach na platformach jak np. Reddit. Stworzenie strony inspirowanej 4chan, taki jakby blog czy coś
+                            TEMAT: GYGŻYDŁO <br/>
+                            Rozszerzenie projektu z pracowni Artbook o narrację transmedialną. <br/>
+                            Stworzenie własnej miejskiej legendy / creepypasty popartej istnieniem w sieci. Na stronie w odpowiednim miejscu trzeba wpisywać hasła, które będą odblokowywały dostęp do kolejnych części historii i tworzonych do nich ilustracji. <br/>
+                            Hasła będą pojawiały się na plakatach rozieszonych w przestrzeni publicznej oraz w postach na social mediach. Trzeba będzie je rozszyfrować. <br/>
+                            Projekt zostanie podsumowany stworzeniem fizycznego artbooka w postaci katalogu / encyklopedii, gdzie linorytowe ilustracje będą opisane odpowiednimi historiami. <br/>
+
                         </p>
                     </div></li>
                     <li><div>
                         <p>
-                            INSPIRACJE: Systemy Interfejsów - 9mother9horse9eyes, SCP Foundation</p>
+                            INSPIRACJE: "Systemy Interfejsów" - _9mother9horse9eyes, SCP Foundation</p>
                     </div></li>
                     <li><div>
                         <p>
-                            RESEARCH: Promocje nowych albumów Twenty One Pliots, nowego sezonu Mr Robot, komentarze na Reddicie użytkownika 9mother9horse9eyes, Capture the Flag</p>
+                            RESEARCH: Promocje nowych albumów Twenty One Pilots, nowego sezonu Mr Robot, komentarze na Reddicie użytkownika 9mother9horse9eyes, Capture the Flag</p>
                     </div></li>
                     <li><div>
                         <p>
@@ -28,13 +37,16 @@ const Description = () => {
                     </div></li>
                     <li><div>
                         <p>
-                            NARZĘDZIA/ OPROGRAMOWANIE: Strona www, Reddit, Facebook, X, Threads, TikTok</p>
+                            NARZĘDZIA / OPROGRAMOWANIE: Strona www, Reddit, Facebook, X, Threads, TikTok</p>
                     </div></li>
                     <li><div>
                         <p>
                             FORMA PREZENTACJI PROJEKTU: Prezentacja dokumentująca poszczególne etapy projektu, artbook, strona internetowa</p>
                     </div></li>
                 </ol>
+                <div>
+                    <button className='uppercase border p-[5px] hover:border-white hover:text-white' onClick={ReloadPage}>Back</button>
+                </div>
             </div>
         </div>
     )
