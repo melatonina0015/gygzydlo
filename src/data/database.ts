a6187528b6e32f5d37ca66e38c4021c8d828dd45
melatonina0015 <kwam0015@gmail.com>
2026-01-28 13:22:18 +0100

    new entries

--- src/data/database.ts
+++ src/data/database.ts
@@ -19,5 +19,42 @@ export const DATA_ENTRIES: Entry[] = [
         date: '02-11-2013',
         password: 'zaburzenie',
         image: "entry002.png"
+    },
+    {
+        id: "003",
+        date: '12-12-2013',
+        password: 'katastrofa',
+        image: "entry003.png"
+    },
+    {
+        id: "004",
+        date: '02-03-2014',
+        password: 'milicja',
+        image: "entry004.png"
+    },
+    {
+        id: "005",
+        date: '27-04-2014',
+        password: 'makapaka',
+        image: "entry005.png"
+    },
+    {
+        id: "006",
+        date: '07-09-2014',
+        password: 'kurcze',
+        image: "entry006.png"
+    },
+    {
+        id: "007",
+        date: '11-04-2017',
+        password: 'mamałyga',
+        image: "entry007.png"
+    },
+    {
+        id: "008",
+        date: '31-08-2019',
+        password: 'korfanty',
+        image: "entry008.png"
     }
+
 ];
